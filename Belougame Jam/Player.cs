97b2718382c00c7884bf405f73cf9fc57f6c8112
Valentin Robert <valentin.robert.42@gmail.com>
2017-08-19 16:20:05 -0700

    now running better

--- Belougame Jam/Player.cs
+++ Belougame Jam/Player.cs
@@ -5,15 +5,28 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Belougame_Jam
 {
+    public enum PlayerDirection
+    {
+        FacingLeft = 0,
+        FacingRight = 1
+    }
+
+    public enum PlayerState
+    {
+        Idle = 0,
+        Running = 1
+    }   
+
     class Player
     {
         public Animation IdleAnimation;
         public Animation RunAnimation;
         public Vector2 Position;
         public bool Active;
-        public bool IsRunning;
         public int Health;
         public float Speed;
+        public PlayerDirection Direction;
+        public PlayerState State;
 
         public void Initialize(
             Animation idleAnimation,
@@ -26,21 +39,34 @@ namespace Belougame_Jam
             Position = position;
             Active = true;
             Health = 100;
-            IsRunning = false;
+            Direction = PlayerDirection.FacingRight;
+            State = PlayerState.Idle;
             Speed = 1;
         }
 
         public void Update(GameTime gameTime, KeyboardState keyboardState)
         {
             float direction = 0;
-
             if (keyboardState.IsKeyDown(Keys.D)) { direction += 1; }
             if (keyboardState.IsKeyDown(Keys.A)) { direction -= 1; }
 
-            IsRunning = direction != 0;
+            if (direction == 0)
+            {
+                State = PlayerState.Idle;
+            }
+            else if (direction > 0)
+            {
+                Direction = PlayerDirection.FacingRight;
+                State = PlayerState.Running;
+            } else
+            {
+                Direction = PlayerDirection.FacingLeft;
+                State = PlayerState.Running;
+            }
 
             Position.X += direction * Speed;
-            SpriteEffects effects = direction >= 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+
+            SpriteEffects effects = directionEffects(Direction);            
             IdleAnimation.Position = Position;
             IdleAnimation.Update(gameTime, effects);
             RunAnimation.Position = Position;
@@ -49,8 +75,27 @@ namespace Belougame_Jam
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            Animation displayedAnimation = IsRunning ? RunAnimation : IdleAnimation;
-            displayedAnimation.Draw(spriteBatch);
+            getAnimation().Draw(spriteBatch);
+        }
+
+        private Animation getAnimation()
+        {
+            switch (State)
+            {
+                case PlayerState.Idle: return IdleAnimation;
+                case PlayerState.Running: return RunAnimation;
+                default: throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private SpriteEffects directionEffects(PlayerDirection d)
+        {
+            switch (Direction)
+            {
+                case PlayerDirection.FacingLeft: return SpriteEffects.FlipHorizontally;
+                case PlayerDirection.FacingRight: return SpriteEffects.None;
+                default: throw new ArgumentOutOfRangeException();
+            };
         }
     }
 }
