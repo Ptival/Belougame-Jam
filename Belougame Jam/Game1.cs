336e9f3e703c08f455d5849322656c3506e11305
13kill <hedi.soumri@gmail.com>
2017-08-19 22:39:18 +0200

    Added : Michel sprite + idle movement

--- Belougame Jam/Game1.cs
+++ Belougame Jam/Game1.cs
@@ -15,11 +15,19 @@ namespace Belougame_Jam
         const int JOHNSON_FRAMES = 8;
         const int JOHNSON_FRAMETIME = 95;
 
+        const string MICHEL_IDLE_FILE = "michel_idle";
+        const int MICHEL_WIDTH = 19;
+        const int MICHEL_HEIGHT = 34;
+        const int MICHEL_FRAMES = 12;
+        const int MICHEL_FRAMETIME = 90;
+
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         // Texture2D playerTexture;
         Texture2D johnsonTexture;
+        Texture2D michelTexture;
         Player player;
+        Player playerMichel;
 
         public Game1()
         {
@@ -37,6 +45,8 @@ namespace Belougame_Jam
         {
             // TODO: Add your initialization logic here
             player = new Player();
+            playerMichel = new Player();
+
 
             base.Initialize();
         }
@@ -50,16 +60,27 @@ namespace Belougame_Jam
             spriteBatch = new SpriteBatch(GraphicsDevice);
             // Create a new SpriteBatch, which can be used to draw textures.
             Animation playerAnimation = new Animation();
+            Animation playerMichelAnimation = new Animation();
+
             johnsonTexture = Content.Load<Texture2D>(JOHNSON_FILE);
             playerAnimation.Initialize(
                 johnsonTexture, Vector2.Zero,
                 JOHNSON_WIDTH, JOHNSON_HEIGHT, JOHNSON_FRAMES, JOHNSON_FRAMETIME,
-                Color.White, 5, true
+                Color.White, 1, true
                 );
+            michelTexture = Content.Load<Texture2D>(MICHEL_IDLE_FILE);
+            playerMichelAnimation.Initialize(
+                michelTexture, Vector2.Zero,
+                MICHEL_WIDTH, MICHEL_HEIGHT, MICHEL_FRAMES, MICHEL_FRAMETIME,
+                Color.White, 2, true
+                );
+
             Vector2 playerPosition = new Vector2(
                 GraphicsDevice.Viewport.TitleSafeArea.X + GraphicsDevice.Viewport.TitleSafeArea.Width / 2,
                 GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
             player.Initialize(playerAnimation, playerPosition);
+            playerMichel.Initialize(playerMichelAnimation, playerPosition);
+
         }
 
         /// <summary>
@@ -83,6 +104,7 @@ namespace Belougame_Jam
             base.Update(gameTime);
 
             player.Update(gameTime);
+            playerMichel.Update(gameTime);
         }
 
         /// <summary>
@@ -96,6 +118,7 @@ namespace Belougame_Jam
             // TODO: Add your drawing code here
             spriteBatch.Begin();
             player.Draw(spriteBatch);
+            playerMichel.Draw(spriteBatch);
             spriteBatch.End();
 
             base.Draw(gameTime);
